36d58cc8c25cf3e24d177d322c4d511dfad688fa
VishnupriyaErakasani <ekrishnaveni2005@gmail.com>
2025-10-15 18:46:01 +0530

    adding docker,json and js files

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require("http");
+
+const PORT = process.env.PORT || 8080;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello from project Docker container!\n");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+  
